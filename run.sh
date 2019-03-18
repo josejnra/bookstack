2fb89aacc7ef98fc4b5143e8e085c0198d35cbf2
jose nunes <josenunesra@gmail.com>
2019-03-17 23:08:35 -0300

    accessing mysql in docker host from container

--- run.sh
+++ run.sh
@@ -1,11 +1,22 @@
-docker run -it \
--e DB_HOST=127.0.0.1 \
+# Find docker host ip
+# ip addr show docker0
+# this may work 
+DOCKER_HOST=$(ip addr show docker0 | grep -i inet | grep -i 16 | cut -d "/" -f 1 | cut -d ' ' -f 6)
+
+
+# In mysql conf (/etc/mysql/mysql.conf.d/mysqld.cnf) bind address to host ip from docker0
+# example
+# bind-address    = 172.17.0.1
+
+# verify user access rules in mysql-server for host options to access using the above host ip
+
+docker run --rm -it \
+-e DB_HOST=$DOCKER_HOST \
 -e DB_DATABASE=bookstack \
 -e DB_USERNAME=root \
 -e DB_PASSWORD=123 \
 -v /home/jose/Projects/docker-images/bookstack/uploads:/var/www/bookstack/public/uploads \
 -v /home/jose/Projects/docker-images/bookstack/storage-uploads:/var/www/bookstack/public/storage \
 -p 8080:80 \
---network=host \
---name test_bookstack \
+--name bookstack-app \
 solidnerd/bookstack:0.25.2
